a98271d7bd8f70583589ab9d7a852aaf1973d1a6
Nanda Dias <fernanda.dias@konduto.com>
2020-06-08 02:54:05 -0300

    Altera o layout da home

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,29 +1,41 @@
 import React from "react";
-import { FiLogIn } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { FaBeer, FaHeart } from "react-icons/fa";
 
 import "./styles.css";
-import logo from "../../assets/logo.svg";
+import logo from "../../assets/rockme-logo.svg";
+import avatars from "../../assets/ilustra-home.png";
 
 const Home = () => {
   return (
     <div id="page-home">
       <div className="content">
-        <header>
-          <img src={logo} alt="Ecoleta" />
-        </header>
-
         <main>
-          <h1>Seu marktplace de coleta de resíduos.</h1>
-          <p>Ajudamos pessoas a encontrarem pontos de coleta</p>
+          <header>
+            <img src={logo} alt="Rock.Me" />
+          </header>
+          <h1>Let’s rock, baby.</h1>
+          <p>O amor da sua vida pode estar a uma música de distância.</p>
 
           <Link to="/create-point">
-            <span>
-              <FiLogIn />
-            </span>
-            <strong>Cadastre um ponto de coleta</strong>
+            <strong>Criar perfil</strong>
           </Link>
         </main>
+        <div className="avatars">
+          <img src={avatars} alt="Rock.Me avatars" />
+        </div>
+      </div>
+      <div className="footer">
+        <p>
+          feito pela{" "}
+          <Link to="https://github.com/fernandadias/" target="_blank">
+            nanda
+          </Link>{" "}
+          com muita <FaBeer /> e <FaHeart /> durante a{" "}
+          <Link to="https://nextlevelweek.com/" target="_blank">
+            next level week 1.0
+          </Link>
+        </p>
       </div>
     </div>
   );
